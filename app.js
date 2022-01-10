68009cb0789621d2b1361bfe2fc1bc5a9c082092
amartya-rs <saha.amartya@outlook.com>
2022-01-10 12:47:39 +0530

    refactor: ES6+ syntax

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-var initialPrice = document.querySelector("#initial-price");
-var quantity = document.querySelector("#stocks-quantity");
-var currentPrice = document.querySelector("#current-price");
-var calculate = document.querySelector("#calculate-btn");
-var outputdiv = document.querySelector("#output");
-var mainContainer = document.querySelector("#main");
+const initialPrice = document.querySelector("#initial-price");
+const quantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const calculate = document.querySelector("#calculate-btn");
+const outputdiv = document.querySelector("#output");
+const mainContainer = document.querySelector("#main"); //doubt about const
 
-calculate.addEventListener("click", clickHandler);
 
 //click handler
-function clickHandler() {
-  var total = 0;
-  var profitPercent = 0;
+let clickHandler = () => {
+  let total = 0;
+  let profitPercent = 0;
 
-  var amtInvested = initialPrice.value * quantity.value;
-  var currentValue = currentPrice.value * quantity.value;
+  let amtInvested = initialPrice.value * quantity.value;
+  let currentValue = currentPrice.value * quantity.value;
 
   total = currentValue - amtInvested;
   profitPercent = (total / amtInvested) * 100;
 
   if (total === 0) {
-    outputdiv.innerText = "No pain no gain. Keep HUSTLING!";
+    outputdiv.innerText = `No pain no gain. Keep HUSTLING!`;
   } else {
     if (total > 0) {
-      outputdiv.innerText =
-        "Your profit is ₹ " +
-        total +
-        " and profit percentage is " +
-        profitPercent +
-        " %";
-        mainContainer.style.backgroundColor = "green";
+      outputdiv.innerText = `Your profit is ₹ ${total} and profit percentage is ${profitPercent} %`;
+      mainContainer.style.backgroundColor = "green";
     } else {
-      outputdiv.innerText =
-        "Your loss is ₹ " +
-        Math.abs(total) +
-        " and loss percentage is " +
-        Math.abs(profitPercent) +
-        " %";
-        mainContainer.style.backgroundColor = "red";
+      outputdiv.innerText = `Your loss is ₹ ${Math.abs(total)} and loss percentage is ${Math.abs(profitPercent)} %`;
+      mainContainer.style.backgroundColor = "red";
     }
   }
-}
+};
+
+calculate.addEventListener("click", clickHandler);//if we push the before the functin def, code breaks
